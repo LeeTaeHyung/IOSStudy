7cc46a3a1b4fb256c00086a8f8706bc586d0879a
TaeHyungLee(Ian Lee) <taehyung.lee@fashiongo.net>
2017-09-18 21:24:41 +0900

    [Ian Lee] Initial commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Lee Ian on 2017. 9. 18..
+//  Copyright © 2017년 Lee Ian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib. thank you
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
